05008a0557ff5ef34bf26a1f0392b6c0b6e9142b
Purlox <purlox@gmail.com>
2016-03-23 21:37:45 +0100

    Added data definition and some basic functions.
    More precisely added the data definition for FunMap and added these functions: empty, singleton, insert, delete, toList.

---
+++ Source/Data/FunMap.hs
@@ -0,0 +1,40 @@
+module FunMap (
+    empty,
+    singleton,
+    
+    insert,
+    delete,
+    
+    toList
+) where
+
+data FunMap k v = FunMap (k -> Maybe v)
+
+
+empty :: FunMap k v
+empty = FunMap f
+    where f _ = Nothing
+
+
+singleton :: Eq k => k -> v -> FunMap k v
+singleton key value = FunMap f
+    where f x | x == key = Just value
+          f _            = Nothing
+
+
+insert :: Eq k => k -> v -> FunMap k v -> FunMap k v
+insert key value (FunMap f) = FunMap g
+    where g x | x == key  = Just value
+              | otherwise = f x
+
+
+delete :: Eq k => k -> FunMap k v -> FunMap k v
+delete key (FunMap f) = FunMap g
+    where g x | x == key  = Nothing
+              | otherwise = f x
+
+
+toList :: (Enum k) => FunMap k v -> [(k, v)]
+toList (FunMap f) = [ (x, value) | x <- enumAll, Just value <- [f x] ]
+    where enumAll = enumFrom (toEnum 0)
+
